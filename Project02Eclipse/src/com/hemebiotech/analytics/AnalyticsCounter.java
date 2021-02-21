b36b94c38564a90a98842132915fc62404940236
David Valente <dvalente77@gmail.com>
2021-02-21 18:44:09 +0100

    add map methode

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,5 +1,26 @@
 package com.hemebiotech.analytics;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class AnalyticsCounter {
 	
+	/**
+	 * Counts the number of occurrences symptoms
+	 * 
+	 * @param listDuplicate
+	 * @return map of symptom name and symptom count
+	 */
+	public Map<String,Integer> map(List<String> listDuplicate) {
+		Map<String,Integer> mapCounter = new HashMap<>();
+		Set<String> symptomsNoDuplicate = new HashSet<>(listDuplicate);
+		for (String symptom : symptomsNoDuplicate) {
+			mapCounter.put(symptom, Collections.frequency(listDuplicate, symptom));
+		}
+		return mapCounter;
+	}
 }
